b44d89876f69509cb24e5fe9f3da116c8818092e
sss <sss@sss>
2018-05-29 13:33:09 +0200

    Creo DTO de TAG

---
+++ quiz-back-feature-init (2)/src/main/java/com/dbg/quizback/dto/TagDTO.java
@@ -0,0 +1,14 @@
+package com.dbg.quizback.dto;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class TagDTO implements Serializable{
+	
+	private Integer idTag;
+	private String tag;
+	//de momento no voy a meter nada de QUiz aquí
+
+}
