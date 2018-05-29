260565a0baced12aff048f9fc733f9db2246f1bd
sss <sss@sss>
2018-05-29 13:32:46 +0200

    Creo service e implementacion de servce para TAG

---
+++ quiz-back-feature-init (2)/src/main/java/com/dbg/quizback/service/TagServiceImpl.java
@@ -0,0 +1,53 @@
+package com.dbg.quizback.service;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.dbg.quizback.dao.TagDAO;
+import com.dbg.quizback.model.Tag;
+
+@Service
+public class TagServiceImpl  implements TagService {
+
+	@Autowired
+	TagDAO tagDao;
+	
+	@Override
+	public Tag create(Tag t) {
+		// TODO Auto-generated method stub
+		return tagDao.save(t);
+	}
+
+	@Override
+	public void update(Tag t) {
+		tagDao.save(t);
+		
+	}
+
+	@Override
+	public Optional<Tag> findById(Integer id) {
+		
+		return tagDao.findById(id);
+	}
+
+	@Override
+	public Set<Tag> findAll(Pageable p) {
+		int page = p.getPageNumber();
+		int size = p.getPageSize();
+		return tagDao.findAll(PageRequest.of(page, size)).stream().collect(Collectors.toSet());
+		
+	}
+
+	@Override
+	public void delete(Tag t) {
+		tagDao.delete(t);
+		
+	}
+
+}
