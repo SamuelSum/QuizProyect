7f6e9056bba81aa98dc21cf30d5e9289b9b14616
sss <sss@sss>
2018-05-30 12:17:44 +0200

    faltaba el @COmponent!!!

--- quiz-back-feature-init (2)/src/main/java/com/dbg/quizback/component/mapper/course/CourseMapperImpl.java
+++ quiz-back-feature-init (2)/src/main/java/com/dbg/quizback/component/mapper/course/CourseMapperImpl.java
@@ -1,9 +1,12 @@
 package com.dbg.quizback.component.mapper.course;
 
+import org.springframework.stereotype.Component;
+
 import com.dbg.quizback.component.mapper.AbstractMapper;
 import com.dbg.quizback.dto.CourseDTO;
 import com.dbg.quizback.model.Course;
 
+@Component
 public class CourseMapperImpl extends AbstractMapper<Course, CourseDTO> implements CourseMapper{
 
 	@Override
